03abf752bfa234fc19e3e7220fc29092208a4bae
heko <91366057+hkohko@users.noreply.github.com>
2023-09-20 18:18:22 +0700

    finished main tiles draft

---
+++ assets/WIP/level/draft/draft_tileset.tsx
@@ -0,0 +1,34 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="draft_tileset" tilewidth="64" tileheight="64" tilecount="10" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="8">
+  <image width="64" height="64" source="../../tileset/tiles_topleft.png"/>
+ </tile>
+ <tile id="9">
+  <image width="64" height="64" source="../../tileset/tiles3.png"/>
+ </tile>
+ <tile id="10">
+  <image width="64" height="64" source="../../tileset/tiles_topright.png"/>
+ </tile>
+ <tile id="11">
+  <image width="64" height="64" source="../../tileset/bg.png"/>
+ </tile>
+ <tile id="14">
+  <image width="64" height="64" source="../../tileset/tiles_vertical_left.png"/>
+ </tile>
+ <tile id="15">
+  <image width="64" height="64" source="../../tileset/tiles_vertical_right.png"/>
+ </tile>
+ <tile id="16">
+  <image width="64" height="64" source="../../player/slime3.png"/>
+ </tile>
+ <tile id="17">
+  <image width="64" height="64" source="../../tileset/tiles3_inverted.png"/>
+ </tile>
+ <tile id="18">
+  <image width="64" height="64" source="../../tileset/tiles_bottomleft.png"/>
+ </tile>
+ <tile id="19">
+  <image width="64" height="64" source="../../tileset/tiles_bottomright.png"/>
+ </tile>
+</tileset>
