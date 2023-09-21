7a9f94dbcbcb4d6e639a63a6c8733cc0dbe307a9
n0Oo0Oo0b <bobthebuilder10492@gmail.com>
2023-09-21 20:09:42 +0800

    Make constants.py more compact

--- pyweek36/constants.py
+++ pyweek36/constants.py
@@ -5,68 +5,43 @@ ASSETS_DIR = Path(__file__).parent.parent / "assets"
 SCREEN_TITLE = "PyMunk Platformer"
 
 # How big are our image tiles?
-SPRITE_IMAGE_SIZE = 64
+SPRITE_IMAGE_SIZE = 32
 
-# Scale sprites up or down
 SPRITE_SCALING_PLAYER = 1
 SPRITE_SCALING_TILES = 1
 
 # Scaled sprite size for tiles
-SPRITE_SIZE = int(SPRITE_IMAGE_SIZE * SPRITE_SCALING_PLAYER)
+SPRITE_SIZE = int(SPRITE_IMAGE_SIZE * SPRITE_SCALING_TILES)
 
-# Size of grid to show on screen, in number of tiles
 SCREEN_GRID_WIDTH = 25
 SCREEN_GRID_HEIGHT = 15
 
-# Size of screen to show, in pixels
 SCREEN_WIDTH = SPRITE_SIZE * SCREEN_GRID_WIDTH
 SCREEN_HEIGHT = SPRITE_SIZE * SCREEN_GRID_HEIGHT
 
-# --- Physics forces. Higher number, faster accelerating.
-
-# Gravity
+# Physics
 GRAVITY = 1500
-
-# Damping - Amount of speed lost per second
 DEFAULT_DAMPING = 1.0
-PLAYER_DAMPING = 2
-
-# Friction between objects
-PLAYER_FRICTION = 2
 WALL_FRICTION = 0.7
-DYNAMIC_ITEM_FRICTION = 0.6
 
-# Mass (defaults to 1)
-PLAYER_MASS = 2.0
+# Player
+PLAYER_MASS = 20
+PLAYER_FRICTION = 0.3
+PLAYER_DAMPING = 0.9
 
-# Keep player from going too fast
-PLAYER_MAX_HORIZONTAL_SPEED = 350
-PLAYER_MAX_VERTICAL_SPEED = 700
-
-# Force applied while on the ground
-PLAYER_MOVE_FORCE_ON_GROUND = 6000
-
-# Force applied when moving left/right in the air
-PLAYER_MOVE_FORCE_IN_AIR = 2000
-
-# Strength of a jump
+PLAYER_HORIZONTAL_SPEED = 350
+PLAYER_ACCEL = 180
+PLAYER_DECEL = 250
+PLAYER_AIR_ACCEL_FACTOR = 0.3
 PLAYER_JUMP_IMPULSE = 1200
 
-# Close enough to not-moving to have the animation go to idle.
+# Player animation
 DEAD_ZONE = 0.1
-
-# Constants used to track if the player is facing left or right
 RIGHT_FACING = 0
 LEFT_FACING = 1
-
-# How many pixels to move before we change the texture in the walking animation
 DISTANCE_TO_CHANGE_TEXTURE = 20
 
-# How much force to put on the bullet
+# Bullet
 BULLET_MOVE_FORCE = 4500
-
-# Mass of the bullet
 BULLET_MASS = 0.1
-
-# Make bullet less affected by gravity
 BULLET_GRAVITY = 300
