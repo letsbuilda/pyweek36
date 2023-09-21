0d6dae2dc78aed17815e4e099be48b4ae4bd4b46
heko <91366057+hkohko@users.noreply.github.com>
2023-09-21 12:00:22 +0700

    added cave walls, organized folders

--- assets/WIP/level/draft/draft_tileset.tsx
+++ assets/WIP/level/draft/draft_tileset.tsx
@@ -1,34 +1,55 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="draft_tileset" tilewidth="64" tileheight="64" tilecount="10" columns="0">
+<tileset version="1.10" tiledversion="1.10.2" name="draft_tileset" tilewidth="64" tileheight="64" tilecount="17" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="8">
-  <image width="64" height="64" source="../../tileset/tiles_topleft.png"/>
+  <image width="64" height="64" source="../../tileset/permanent_blocks/tiles_topleft.png"/>
  </tile>
  <tile id="9">
-  <image width="64" height="64" source="../../tileset/tiles3.png"/>
+  <image width="64" height="64" source="../../tileset/permanent_blocks/tiles3.png"/>
  </tile>
  <tile id="10">
-  <image width="64" height="64" source="../../tileset/tiles_topright.png"/>
+  <image width="64" height="64" source="../../tileset/permanent_blocks/tiles_topright.png"/>
  </tile>
  <tile id="11">
-  <image width="64" height="64" source="../../tileset/bg.png"/>
+  <image width="64" height="64" source="../../tileset/permanent_blocks/bg.png"/>
  </tile>
  <tile id="14">
-  <image width="64" height="64" source="../../tileset/tiles_vertical_left.png"/>
+  <image width="64" height="64" source="../../tileset/permanent_blocks/tiles_vertical_left.png"/>
  </tile>
  <tile id="15">
-  <image width="64" height="64" source="../../tileset/tiles_vertical_right.png"/>
+  <image width="64" height="64" source="../../tileset/permanent_blocks/tiles_vertical_right.png"/>
  </tile>
  <tile id="16">
   <image width="64" height="64" source="../../player/slime3.png"/>
  </tile>
  <tile id="17">
-  <image width="64" height="64" source="../../tileset/tiles3_inverted.png"/>
+  <image width="64" height="64" source="../../tileset/permanent_blocks/tiles3_inverted.png"/>
  </tile>
  <tile id="18">
-  <image width="64" height="64" source="../../tileset/tiles_bottomleft.png"/>
+  <image width="64" height="64" source="../../tileset/permanent_blocks/tiles_bottomleft.png"/>
  </tile>
  <tile id="19">
-  <image width="64" height="64" source="../../tileset/tiles_bottomright.png"/>
+  <image width="64" height="64" source="../../tileset/permanent_blocks/tiles_bottomright.png"/>
+ </tile>
+ <tile id="20">
+  <image width="64" height="64" source="../../tileset/permanent_blocks/connector_tile_bottomleft.png"/>
+ </tile>
+ <tile id="21">
+  <image width="64" height="64" source="../../tileset/permanent_blocks/connector_tile_bottomright.png"/>
+ </tile>
+ <tile id="22">
+  <image width="64" height="64" source="../../tileset/permanent_blocks/connector_tile_upleft.png"/>
+ </tile>
+ <tile id="23">
+  <image width="64" height="64" source="../../tileset/permanent_blocks/connector_tile_upright.png"/>
+ </tile>
+ <tile id="29">
+  <image width="64" height="64" source="../../tileset/air_blocks/air_block.png"/>
+ </tile>
+ <tile id="30">
+  <image width="64" height="64" source="../../tileset/air_blocks/air_block2.png"/>
+ </tile>
+ <tile id="31">
+  <image width="64" height="64" source="../../tileset/cave_wall/cave_wall.png"/>
  </tile>
 </tileset>
