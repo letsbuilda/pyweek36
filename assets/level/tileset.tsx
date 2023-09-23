19a302b8e6d9864fa773bea6a036ddbe12466c5b
heko <91366057+hkohko@users.noreply.github.com>
2023-09-23 09:48:59 +0700

    reorganized folders

---
+++ assets/level/tileset.tsx
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tileset" tilewidth="64" tileheight="64" tilecount="16" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="../blocks/permanent_blocks/bg.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="../blocks/permanent_blocks/connector_tile_bottomleft.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="../blocks/permanent_blocks/connector_tile_bottomright.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="../blocks/permanent_blocks/connector_tile_upleft.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="../blocks/permanent_blocks/connector_tile_upright.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="64" source="../blocks/permanent_blocks/tiles.png"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="64" source="../blocks/permanent_blocks/tiles_bottomleft.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="64" source="../blocks/permanent_blocks/tiles_bottomright.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="64" source="../blocks/permanent_blocks/tiles_inverted.png"/>
+ </tile>
+ <tile id="9">
+  <image width="64" height="64" source="../blocks/permanent_blocks/tiles_topleft.png"/>
+ </tile>
+ <tile id="10">
+  <image width="64" height="64" source="../blocks/permanent_blocks/tiles_topright.png"/>
+ </tile>
+ <tile id="11">
+  <image width="64" height="64" source="../blocks/permanent_blocks/tiles_vertical_left.png"/>
+ </tile>
+ <tile id="12">
+  <image width="64" height="64" source="../blocks/permanent_blocks/tiles_vertical_right.png"/>
+ </tile>
+ <tile id="13">
+  <image width="64" height="64" source="../blocks/dark_matter_source/dark_matter_source1.png"/>
+ </tile>
+ <tile id="14">
+  <image width="64" height="64" source="../blocks/dark_matter_blocks/dark_matter_blocks.png"/>
+ </tile>
+ <tile id="15">
+  <image width="64" height="64" source="../blocks/cave_wall/cave_wall.png"/>
+ </tile>
+</tileset>
