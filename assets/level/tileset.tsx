9510e2eaf8a31b558c6355a8760bad3a951e692d
heko <91366057+hkohko@users.noreply.github.com>
2023-09-23 10:12:58 +0700

    created new levels

--- assets/level/tileset.tsx
+++ assets/level/tileset.tsx
@@ -1,49 +1,49 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="tileset" tilewidth="64" tileheight="64" tilecount="16" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
+ <tile id="0" type="solid">
   <image width="64" height="64" source="../blocks/permanent_blocks/bg.png"/>
  </tile>
- <tile id="1">
+ <tile id="1" type="solid">
   <image width="64" height="64" source="../blocks/permanent_blocks/connector_tile_bottomleft.png"/>
  </tile>
- <tile id="2">
+ <tile id="2" type="solid">
   <image width="64" height="64" source="../blocks/permanent_blocks/connector_tile_bottomright.png"/>
  </tile>
- <tile id="3">
+ <tile id="3" type="solid">
   <image width="64" height="64" source="../blocks/permanent_blocks/connector_tile_upleft.png"/>
  </tile>
- <tile id="4">
+ <tile id="4" type="solid">
   <image width="64" height="64" source="../blocks/permanent_blocks/connector_tile_upright.png"/>
  </tile>
- <tile id="5">
+ <tile id="5" type="solid">
   <image width="64" height="64" source="../blocks/permanent_blocks/tiles.png"/>
  </tile>
- <tile id="6">
+ <tile id="6" type="solid">
   <image width="64" height="64" source="../blocks/permanent_blocks/tiles_bottomleft.png"/>
  </tile>
- <tile id="7">
+ <tile id="7" type="solid">
   <image width="64" height="64" source="../blocks/permanent_blocks/tiles_bottomright.png"/>
  </tile>
- <tile id="8">
+ <tile id="8" type="solid">
   <image width="64" height="64" source="../blocks/permanent_blocks/tiles_inverted.png"/>
  </tile>
- <tile id="9">
+ <tile id="9" type="solid">
   <image width="64" height="64" source="../blocks/permanent_blocks/tiles_topleft.png"/>
  </tile>
- <tile id="10">
+ <tile id="10" type="solid">
   <image width="64" height="64" source="../blocks/permanent_blocks/tiles_topright.png"/>
  </tile>
- <tile id="11">
+ <tile id="11" type="solid">
   <image width="64" height="64" source="../blocks/permanent_blocks/tiles_vertical_left.png"/>
  </tile>
- <tile id="12">
+ <tile id="12" type="solid">
   <image width="64" height="64" source="../blocks/permanent_blocks/tiles_vertical_right.png"/>
  </tile>
- <tile id="13">
+ <tile id="13" type="source">
   <image width="64" height="64" source="../blocks/dark_matter_source/dark_matter_source1.png"/>
  </tile>
- <tile id="14">
+ <tile id="14" type="darkmatter">
   <image width="64" height="64" source="../blocks/dark_matter_blocks/dark_matter_blocks.png"/>
  </tile>
  <tile id="15">
