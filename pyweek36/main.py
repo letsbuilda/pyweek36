db6c162feee9909208fc81aced4086191adf060a
Anonymous4045 <contact@anonymous4045.social>
2023-09-24 00:18:57 -0500

    Make theme loop

--- pyweek36/main.py
+++ pyweek36/main.py
@@ -4,14 +4,28 @@ import arcade
 from .constants import MUSIC_PATH
 from .gui import GameGUI
 
+from threading import Thread
+
+
+background_music = arcade.Sound(MUSIC_PATH)
+music_id = background_music.play(volume=0.5)
+
+
+# Game music theme thread plays infinitely
+def play_music():
+    # If music is not playing, play it
+    global background_music, music_id
+    if not background_music.is_playing(music_id):
+        music_id = background_music.play(volume=0.5)
+
 
 def main():
     """Main method"""
     window = GameGUI()
     window.setup()
 
-    # Load music from path
-    music = arcade.Sound(MUSIC_PATH)
-    music.play(volume=0.5)
+    # Start the background music
+    music_thread = Thread(target=play_music)
+    music_thread.start()
 
     arcade.run()
